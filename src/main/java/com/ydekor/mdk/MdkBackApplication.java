370723a44fee85e7c8c3c1ed18579cbd2904b5bf
ydekor <elefont@yandex.ru>
2023-12-23 19:27:05 +0300

    initial commit

---
+++ src/main/java/com/ydekor/mdk/MdkBackApplication.java
@@ -0,0 +1,12 @@
+package com.ydekor.mdk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdkBackApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MdkBackApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/ydekor/mdk/controller/FoodController.java
@@ -0,0 +1,5 @@
+package com.ydekor.mdk.controller;
+
+public class FoodController {
+
+}
